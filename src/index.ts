73fd1182c96c3e567c93c0b485a2ecf88c03a942
toycrane <alwaysfun2183@gmail.com>
2020-09-01 12:04:03 +0900

    dotenv 설정 추가

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import "./env";
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 import { createContext } from "./context";
 
+const PORT = process.env.PORT;
 const server = new ApolloServer({ schema, context: createContext });
 
-server.listen().then(({ url }) => {
-	console.log(`🚀  Server ready at ${url}`);
+server.listen({ port: PORT }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
 });
