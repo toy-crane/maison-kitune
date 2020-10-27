f62ccde032870e5122990dd82c9d0906a6ee13b6
toycrane <alwaysfun2183@gmail.com>
2020-10-27 19:15:33 +0900

    resendVerificationToken API 구현

---
+++ src/gql/User/resendVerificationToken/resendVerificationToken.ts
@@ -0,0 +1,43 @@
+import { IResolvers } from "graphql-tools";
+import { Context } from "../../../context";
+import createRandomToken from "../../../utils/auth/createSecret";
+import { sendActivateAccountEmail } from "../../../utils/mail/sendMail";
+
+const query: IResolvers = {
+  Mutation: {
+    resendVerificationToken: async (_, { email }, ctx: Context) => {
+      const { prisma } = ctx;
+      const users = await prisma.user.findMany({
+        where: {
+          email,
+          isActive: false,
+        },
+      });
+      if (!users.length) {
+        throw Error("잘못된 이메일입니다.");
+      }
+      const clientUrl = process.env.CLIENT_URL;
+      const verificationToken = createRandomToken();
+      await prisma.verificationToken.create({
+        data: {
+          verificationToken,
+          user: {
+            connect: {
+              id: users[0].id,
+            },
+          },
+        },
+      });
+      const activateUrl = `${clientUrl}/activate-account/?verificationToken=${verificationToken}`;
+      try {
+        sendActivateAccountEmail(activateUrl, users[0].email);
+      } catch (e) {
+        console.log(e);
+        throw Error("이메일 발송에 실패했습니다.");
+      }
+      return true;
+    },
+  },
+};
+
+export default query;
